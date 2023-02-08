fbba9d3c5b81faf76896b982495a732112d5919e
Xayah <akaashi1owo@qq.com>
2023-02-08 16:55:16 +0800

    更新了状态

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import axios from 'axios'
+// Vue.use(axios)
+// import Vuex from 'vuex'
+// Vue.use(Vuex)
+// import VueRouter from 'vue-router'
+// Vue.use(VueRouter)
+
+// import router from './router'
 
 Vue.config.productionTip = false
 
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+//引入VueRouter
+import VueRouter from 'vue-router'
+//引入路由组件
+
+
+//创建router实例对象，去管理一组一组的路由规则
+const router = new VueRouter({
+	//👇注意这是routes!!不是routers!!
+    routes:[
+
+	]
+})
+
+//暴露router
+export default router
