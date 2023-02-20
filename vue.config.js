623692de66fa13044bc156ad2697de4619a71aa2
Xayah <akaashi1owo@qq.com>
2023-02-20 19:12:52 +0800

    xixi

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    proxy: 'http://pve.lycoris.site:9780'
+  }
 })
