fba7428534ac99e103518f1a1531c33f4777ec78
Xayah <akaashi1owo@qq.com>
2023-02-20 16:31:09 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -42,9 +42,6 @@
         font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
             "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
     }
-    ::-webkit-scrollbar {
-
-    }
     .el-popup-parent--hidden {
         padding-right: 0px;
     }
